3ec5a933be0c6fe69d899e29f577f13772efeda4
miko <ptechasa@mail.ccsf.edu>
2019-07-10 12:55:44 -0700

    update js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,51 @@
-// Define variable for using in Psychic Game
+// Create variable for using in Psychic Game
 var wins = 0;
 var losses = 0;
-var guesses = 9;
-var userGuessLeft = [];
-var guessRemaining = document.querySelector('#numGuessLeft');
-
-// Random letters a-z
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var guesses;
+var computerGuess;
 
 
-//Computer's random words
-var comRandomLetters = Math.floor(Math.random() * letters.length);
-console.log(letters[comRandomLetters]);
+function playGame() {
 
+    // Computers choose a letter 
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var random = Math.floor(Math.random() * letters.length);
+    computerGuess = letters[random];
+    guesses = 9;
+    console.log("computerGuess ", computerGuess);
 
-//User's picks the words and guess left start from 9
-function userPickWords(event) {
-    guesses--;
-    guessRemaining.innerText = guesses;
-    // console.log(event.key)
-    userGuessLeft.push(event.key);
-    document.querySelector("#guessSoFar").innerText = userGuessLeft;        
 }
-document.onkeypress = userPickWords;
+playGame()
 
+//When user presses any keys ,it launches a function to execute 
+document.onkeypress = function (event) {
 
-// When user and computer choose the same letter
+    //If user and computer choose the same letter
+    //win adds 1
+    //start new play game 
+    if (event.key === computerGuess) {
+        wins++;
+        playGame()
 
+        // If both choose difference
+        // guesses decrease 1
+    } else {
+        guesses--;
+    }
 
+    //If user guesses reach to 0
+    //losses add 1
+    //start to play game 
+    if (guesses == 0) {
+        losses++;
+        playGame()
 
-//When user and computer choose different letter
+    }
+    console.log("guesses ", guesses)
+    console.log("losses ", losses)
+    console.log("wins ", wins)
 
-
-
-
-// User guesses left and reach to 0
-
-
-
+}
 
 
 
