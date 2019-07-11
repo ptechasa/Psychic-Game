11aee159c3d19842d9a3872ab7eae2a671c60931
miko <ptechasa@mail.ccsf.edu>
2019-07-10 17:24:30 -0700

    update html and javascript final verion

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 // Create variable for using in Psychic Game
 var wins = 0;
 var losses = 0;
-var guesses;
+var guesses = 9;
 var computerGuess;
-
+var guessSoFar = [];
 
 function playGame() {
 
@@ -11,7 +11,7 @@ function playGame() {
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var random = Math.floor(Math.random() * letters.length);
     computerGuess = letters[random];
-    guesses = 9;
+    
     console.log("computerGuess ", computerGuess);
 
 }
@@ -25,19 +25,26 @@ document.onkeypress = function (event) {
     //start new play game 
     if (event.key === computerGuess) {
         wins++;
+        document.querySelector('#numWins').innerHTML = wins;
         playGame()
-
         // If both choose difference
         // guesses decrease 1
     } else {
         guesses--;
-    }
+        document.querySelector("#numGuessLeft").innerHTML = guesses;
+        guessSoFar.push(event.key);
+        document.querySelector("#guessSoFar").innerHTML = guessSoFar.join();
+        }
 
     //If user guesses reach to 0
     //losses add 1
     //start to play game 
     if (guesses == 0) {
         losses++;
+        document.querySelector('#numLosses').innerHTML = losses;
+        guesses = 9;
+        guessSoFar = [];
+        document.querySelector("#guessSoFar").innerHTML = guessSoFar.join();
         playGame()
 
     }
